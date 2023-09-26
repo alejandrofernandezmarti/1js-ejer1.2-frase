5d12998313a0b9ada2cd35d731403e6c74d33ece
alejandrofernandezmarti <alefermar16@alu.edu.gva.es>
2023-09-26 11:41:57 +0200

    new

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,31 +1,41 @@
 'use strict'
 
 function letters(cadena) {
+	return cadena.length;
 
 }
 
 function words(cadena) {
-
+	return cadena.split(' ').length;
 }
 
 function upperString(cadena) {
-
+	return cadena.toUpperCase();
 }
 
 function titleString(cadena) {
-
+	let frase = "Una cadena para probar"
+	frase = cadena.split(' ');
+	for (let i = 0; i <= frase.length ; i++) {
+		 let final = frase[i].substring(0,1).toUpperCase();
+		 let mayus = frase[i].substring(1);
+		 frase[i] = mayus + final;
+	}
+	return frase.join(' ');
 }
 
 function backwardsLetters(cadena) {
-
+	return cadena.split('').reverse().join('');
 }
 
 function backwardsWords(cadena) {
-
+return cadena.split(' ').reverse().join(' ');
 }
 
-function palindrome(cadena) {
-
+function palindromo(cadena) {
+    let cadenaReves = cadena.split(' ').reverse().join('').replaceAll(' ','');
+	let cadenaDerecho = cadena.split(' ').join('').replaceAll(' ','');
+	return cadenaReves === cadenaDerecho;
 }
 
 module.exports = {
@@ -35,5 +45,5 @@ module.exports = {
 	titleString,
 	backwardsLetters,
 	backwardsWords,
-	palindrome
+	palindromo
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,14 @@
 'use strict'
+import {backwardsLetters, backwardsWords, letters, palindromo, titleString, upperString, words} from "./functions";
+
+let cadena = prompt("Introduce una cadena")
+
+console.log(cadena)
+console.log(letters(cadena) + " letras y "+words(cadena) + " palabras")
+console.log(upperString(cadena))
+console.log(titleString(cadena))
+console.log(backwardsLetters(cadena))
+console.log(backwardsWords(cadena))
+console.log(palindromo(cadena))
 
 
